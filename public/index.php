7886fb7d258be2da4f7b2cba556f424e19ff3037
krishna <bhattakrishna111@gmail.com>
2022-07-21 22:24:46 +0545

    project structure completed

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
 
 $app = new Application();
 
-$app ->router->get('/',function(){
+$app ->router->get('/',function() {
     return 'hello world';
 });
 
